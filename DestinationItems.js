124d9da57bac861c4295ee8577831eb55de87158
Rohith3211 <121745982+Rohith3211@users.noreply.github.com>
2023-02-25 20:09:21 +0530

    Update DestinationItems.js

--- DestinationItems.js
+++ DestinationItems.js
@@ -1,3 +1,5 @@
+
+//third // 
 import {Component} from 'react'
 import './index.css'
 
@@ -17,3 +19,128 @@ class DestinationItem extends Component {
 
 export default DestinationItem
 
+
+//second //
+import {Component} from 'react'
+import './index.css'
+import DestinationItem from '../DestinationItem'
+
+class DestinationSearch extends Component {
+  state = {searchInput: ''}
+
+  onChangeEvents = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  render() {
+    const {destinationsList} = this.props
+    const {searchInput} = this.state
+    const searchResults = destinationsList.filter(e =>
+      e.name.toLowerCase().includes(searchInput),
+    )
+
+    return (
+      <div className="container">
+        <h1>Destination search</h1>
+
+        <div className="search-div">
+          <input
+            className="input"
+            type="search"
+            placeholder="Search"
+            onChange={this.onChangeEvents}
+            value={searchInput}
+          />
+          <img
+            className="img1"
+            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png "
+            alt="search icon"
+          />
+        </div>
+        <ul className="card">
+          {searchResults.map(each => (
+            <DestinationItem details={each} key={each.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default DestinationSearch
+
+// first //
+import DestinationSearch from './components/DestinationSearch'
+
+import './App.css'
+
+const destinationsList = [
+  {
+    id: 1,
+    name: 'Melaka Mosque',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/melaka-mosque-img.png',
+  },
+  {
+    id: 2,
+    name: 'Shrubland',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/shrubland-img.png',
+  },
+  {
+    id: 3,
+    name: 'New York',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/new-york-img.png',
+  },
+  {
+    id: 4,
+    name: 'Escarpment',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/escarpment-img.png',
+  },
+  {
+    id: 5,
+    name: 'Westminster Abbey',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/westminster-abbey-img.png',
+  },
+  {
+    id: 6,
+    name: 'South Downs National Park',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/south-downs-national-park-img.png',
+  },
+  {
+    id: 7,
+    name: 'National Historic Site',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/national-historic-site-img.png',
+  },
+  {
+    id: 8,
+    name: 'Tower Bridge',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/tower-bridge-img.png',
+  },
+  {
+    id: 9,
+    name: 'Arc Here',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/arc-here-img.png',
+  },
+  {
+    id: 10,
+    name: 'Steeple',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/steeple-img.png',
+  },
+  {
+    id: 11,
+    name: 'Glaciokarst',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/glaciokarst-img.png',
+  },
+  {
+    id: 12,
+    name: 'Parco Nazionale delle Cinque Terre',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/parco-nazionale-delle-cinque-terre-img.png',
+  },
+]
+
+const App = () => <DestinationSearch destinationsList={destinationsList} />
+
+export default App
